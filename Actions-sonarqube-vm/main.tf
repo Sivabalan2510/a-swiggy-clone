6f2cacbd60d1fcc44253e412c6ceee938a14203a
Sivabalan K <sivabtechit25@gmail.com>
2025-04-18 09:53:31 +0530

    checking

--- Actions-sonarqube-vm/main.tf
+++ Actions-sonarqube-vm/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami                    = "ami-084568db4383264d4"      #change ami id for different region
   instance_type          = "t2.large"
   key_name               = "jenpipe"              #change key name as per your setup
-  vpc_security_group_ids = "sg-0f507ce682162d9a1"
+  vpc_security_group_ids = sg-0f507ce682162d9a1
   user_data              = templatefile("./install.sh", {})
 
   tags = {
@@ -13,4 +13,3 @@ resource "aws_instance" "web" {
     volume_size = 40
   }
 }
-
