7953e7041fd9ec19c56f1be06f95a6e04e2d7059
Sivabalan K <sivabtechit25@gmail.com>
2025-04-18 09:55:30 +0530

    corrected the errors

--- Actions-sonarqube-vm/main.tf
+++ Actions-sonarqube-vm/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
   ami                    = "ami-084568db4383264d4"      #change ami id for different region
   instance_type          = "t2.large"
   key_name               = "jenpipe"              #change key name as per your setup
-  vpc_security_group_ids = sg-0f507ce682162d9a1
+  vpc_security_group_ids = ["sg-0f507ce682162d9a1"]
   user_data              = templatefile("./install.sh", {})
 
   tags = {
@@ -12,4 +12,4 @@ resource "aws_instance" "web" {
   root_block_device {
     volume_size = 40
   }
-}
+} 
